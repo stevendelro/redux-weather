e6b0935135cd079f31bf1021406937fcf1515d41
Steven Del Rosario <stevendelro@pm.me>
2020-03-19 20:15:04 -0700

    update api keys

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 // Use your own tokens!
-const mapBoxToken = process.env.REACT_APP_MAPBOX_TOKEN
+const mapBoxToken = 'pk.eyJ1Ijoic3RldmVuZGVscm9zYXJpbyIsImEiOiJjanl2Zndpbmwwb3p3M2lta2xyNjlhc3Q0In0.EUeki9FFRcyDIirOGn26vw'
 const proxy = 'https://cors-anywhere.herokuapp.com/'
-const darkSkyToken = process.env.REACT_APP_DARKSKY_TOKEN
+const darkSkyToken = '49672afaebb7601daeb3e11bb45cc16f'
 
 export const fetchWeather =  location => {
   const mapBoxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
